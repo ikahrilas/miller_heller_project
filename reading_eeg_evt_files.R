d88d8bafbb6566c7415c0886a75543d7e0df885f
Ian Kahrilas <ijk60068@gmail.com>
2022-04-10 08:22:03 -0500

    finalize data processing to prepare for plotting

--- reading_eeg_evt_files.R
+++ reading_eeg_evt_files.R
@@ -16,7 +16,7 @@ library(data.table)
 #+ vector of all files in working directory
 # object with all file names
 ## pids
-pids <- list.files(here("Preprocessed_CW_EEG_Data", "Preprocessed_CTQ_Sample"))[c(-106, -109)]
+pids <- list.files(here("Preprocessed_CW_EEG_Data", "Preprocessed_CTQ_Sample"))[-109]
 
 ## mul files
 mul_files <- map_chr(pids, ~ {
@@ -37,32 +37,38 @@ files_evt <- glue("Preprocessed_CW_EEG_Data/Preprocessed_CTQ_Sample/{pids}/{evt_
 # define vector of number of total possible trials for each to derive proportion of retained trials 
 # for each participant total trials vector
 tot_trials <- c(64, 64, 62, 124, 32, 30, 32, 32, 64, 64, 124, 32, 32, 32, 32)
+# block order vector
+block_order <- c("fixation", 
+                 "congruent_ct",
+                 "incongruent_ct",
+                 "neutral_ct",
+                 "pure_congruent_ct",
+                 "pure_incongruent_ct",
+                 "neutral_congruent_ct",
+                 "neutral_incongruent_ct",
+                 "congruent_at",
+                 "incongruent_at",
+                 "neutral_at",
+                 "pure_congruent_at",
+                 "pure_incongruent_at",
+                 "neutral_congruent_at",
+                 "neutral_incongruent_at")
 
 # prepare parallelization
 plan(multisession, workers = 8)
 
-read_csv2(files_evt[1]) |> 
-  rename("block:n_trials" = `Tmu         \tCode\tTriNo\tComnt`) |>
-  separate(`block:n_trials`, into = c("block", "n_trials"), sep = ":") |>
-  mutate(n_trials = str_remove(n_trials, "avs"),
-         n_trials = as.numeric(n_trials),
-         prop_trials = n_trials / tot_trials)
-## pick up here - refine block variable
-
 # map function to read in data
-evt <- future_map(files_evt, ~ {
-  read_csv2(.x) |>
-    select(-Tmu) |>
-    rename("block:n_trials" = `Code\tTriNo\tComnt`) |>
-    mutate(`block:n_trials` = str_remove(`block:n_trials`, "42\t200000\t")) |>
+evt <- future_map_dfr(files_evt, ~ {
+  read_csv2(.x) |> 
+    rename("block:n_trials" = `Tmu         \tCode\tTriNo\tComnt`) |>
     separate(`block:n_trials`, into = c("block", "n_trials"), sep = ":") |>
-    mutate(n_trials = str_remove(n_trials, "avs"),
+    mutate(pid = as.numeric(str_extract(.x, "[0-9]+")),
+           n_trials = str_remove(n_trials, "avs"),
            n_trials = as.numeric(n_trials),
            prop_trials = n_trials / tot_trials,
-           pid = as.numeric(str_extract(.x, "[0-9]+"))) |>
+           block = block_order) |>
     relocate(pid, everything())
-}) |>
-  reduce(bind_rows)
+})
 
 #' read in mul files as data frames
 #+ read in eeg data
@@ -72,27 +78,27 @@ trial_names <- evt |>
   unique() |> 
   pull()
 
-# read in the data
-eeg_dat <- future_map(files_mul, ~ {
+# read in the mul data
+eeg_dat <- future_map_dfr(files_mul, ~ {
   read_table2(.x, skip = 1) %>% 
     mutate(
       pid = as.numeric(str_extract(.x, "[0-9]{3}")),
       trial_type = rep(trial_names,
                        each = (nrow(.) / length(trial_names))),
-      ms = rep(seq(-200, 1400,
-                   by = ((1600 + (1600 / 400))/ 400)),
+      ms = rep(seq(-200, 1000,
+                   by = ((1200 + (1200 / 300))/ 300)),
                times = 15)) |>
       relocate(pid, trial_type, ms, everything())
 }
-) |> 
-  reduce(bind_rows) |>
-  select(-c(EVENT, SCL, SCR, `VEOG1-M1'`, `EKG`, `VEOG2-M1'`, `M1'-M1'`))
+)
 
-# remove "-M1'" from variable names
-names(eeg_dat) <- str_remove(names(eeg_dat), "-M1'")
+# remove "_avr" from variable names
+names(eeg_dat) <- str_remove(names(eeg_dat), "_avr")
+names(eeg_dat)[64] <- "M1"
 
 #' finally, merge the event and erp data together and output csv file
 #+ merge the data together
 dat <- full_join(evt, eeg_dat, by = "pid")
+
 # write csv file
 write_csv(dat, "data_wide.csv")
